65375ff1cb743963db95521fcd2aea47b270e9ba
Paul Lu <sininglu@sandiego.edu>
2023-03-07 19:00:58 -0800

    added dragging function

--- script.js
+++ script.js
@@ -2,21 +2,20 @@
 
 // press Enter to focus the input field
 document.addEventListener("keypress", (e) => {
-  if(e.key == "Enter"){
+  if (e.key == "Enter") {
     e.preventDefault();
-    document.getElementById('input').focus();
+    document.getElementById("input").focus();
   }
-})
+});
 
 // while in the input field, press Enter to add new todo
-const input = document.getElementById("input"); 
+const input = document.getElementById("input");
 input.addEventListener("keypress", (e) => {
-  if(e.key == "Enter"){
+  if (e.key == "Enter") {
     e.preventDefault();
-    document.getElementById('addBtn').click();
+    document.getElementById("addBtn").click();
   }
-})
-
+});
 
 const addItem = () => {
   const value = document.getElementById("input").value;
@@ -34,25 +33,76 @@ const addItem = () => {
 
 const createNewItem = (value) => {
   const list = document.getElementById("list");
-  const item = document.createElement("li");
+  let item = document.createElement("li");
 
   // Assign unique id
   const uniqId = `${value}-` + new Date().getTime();
   item.setAttribute("id", uniqId);
-  item.setAttribute("class", "item");
+  //   item.setAttribute("class", "item");
+  item.classList.add("item");
   item.innerText = value;
 
+  item = setDragging(item);
 
   const delBtn = document.createElement("div");
   delBtn.setAttribute("class", "cross");
   delBtn.onclick = () => {
     item.remove();
-  }
-  
+  };
+
   item.append(delBtn);
   list.append(item);
 };
 
+// give dragging properties
+const setDragging = (item) => {
+  item.draggable = true;
+
+  // set opacity when dragged
+  item.addEventListener("dragstart", () => {
+    item.classList.add("dragging");
+  });
+
+  item.addEventListener("dragend", () => {
+    item.classList.remove("dragging");
+  });
+
+  return item;
+};
+
+// rearrange elements
+const taskArray = document.getElementById("list");
+taskArray.addEventListener("dragover", (e) => {
+  e.preventDefault();
+  const closest = getDragAfterElement(taskArray, e.clientY);
+  const draggable = document.querySelector(".dragging");
+
+  if (closest) {
+    taskArray.insertBefore(draggable, closest);
+  } else {
+    taskArray.appendChild(draggable);
+  }
+});
+
+const getDragAfterElement = (container, y) => {
+  const placeableElements = [
+    ...container.querySelectorAll(".item:not(.dragging)"),
+  ];
+
+  return placeableElements.reduce(
+    (closest, child) => {
+      const box = child.getBoundingClientRect();
+      const offset = y - box.top - box.height / 2;
+
+      if (offset < 0 && offset > closest.offset) {
+        return { offset: offset, element: child };
+      } else {
+        return closest;
+      }
+    },
+    { offset: Number.NEGATIVE_INFINITY }
+  ).element;
+};
 
 // for css purpose:
 createNewItem("Grocery Shopping");
